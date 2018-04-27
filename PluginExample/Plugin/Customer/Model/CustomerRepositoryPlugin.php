a9dc3b09f0568a2379226786a413d010208f93d5
nans <n.andry.serg@gmail.com>
2018-04-27 20:35:26 +0300

    PluginExample: added around method.

---
+++ PluginExample/Plugin/Customer/Model/CustomerRepositoryPlugin.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Magestudy\PluginExample\Plugin\Customer\Model;
+
+use Magento\Customer\Api\CustomerRepositoryInterface;
+use Magento\Customer\Api\Data\CustomerInterface;
+use Magento\Framework\Message\ManagerInterface;
+
+class CustomerRepositoryPlugin
+{
+    /** @var ManagerInterface */
+    private $messageManager;
+
+    /**
+     * @param ManagerInterface $messageManager
+     */
+    public function __construct(
+        ManagerInterface $messageManager
+    ) {
+        $this->messageManager = $messageManager;
+    }
+
+    public function aroundSave(
+        CustomerRepositoryInterface $subject,
+        callable $proceed,
+        CustomerInterface $customer,
+        $passwordHash = null
+    ): CustomerInterface {
+        $this->messageManager->addNoticeMessage('Message before save customer');
+        $savedCustomer = $proceed($customer, $passwordHash);
+        $this->messageManager->addNoticeMessage('Message after save customer');
+        return $savedCustomer;
+    }
+}
