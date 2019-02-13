fc77b4fac5ae9d2de00da717cbfc069d74f370df
nans <n.andry.serg@gmail.com>
2019-02-13 23:10:20 +0200

    Customjs: added index sample.

--- Customjs/Block/Test/Simple.php
+++ Customjs/Block/Test/Simple.php
@@ -8,8 +8,6 @@ use Magento\Framework\View\Element\Template\Context;
 class Simple extends Template
 {
     /**
-     * Construct
-     *
      * @param Context $context
      * @param array $data
      */
@@ -34,14 +32,15 @@ class Simple extends Template
     public function getUserData()
     {
         $users = [];
-        $users[] = ['id' => 1, 'name' => 'Alex', 'role' => 'User', 'email' => 'AlexUser@mail.com'];
+        $users[] = ['id' => 11, 'name' => 'Alex', 'role' => 'User', 'email' => 'AlexUser@mail.com'];
         $users[] = [
-            'id' => 2,
+            'id' => 12,
             'name' => 'Andrew',
             'role' => 'Administrator',
             'email' => 'AndrewAdmin@mail.com'
         ];
-        $users[] = ['id' => 3, 'name' => 'Bob', 'role' => 'Moderator', 'email' => 'BobModerator@mail.com'];
+        $users[] = ['id' => 13, 'name' => 'Bob', 'role' => 'Moderator', 'email' => 'BobModerator@mail.com'];
+
         return \Zend_Json_Encoder::encode($users);
     }
 }
