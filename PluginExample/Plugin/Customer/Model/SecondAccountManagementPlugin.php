9d54008cfbf0c3443f668ab0adcb95f25a265382
nans <n.andry.serg@gmail.com>
2019-12-28 20:16:11 +0200

    Added sortOrder for plugin

--- PluginExample/Plugin/Customer/Model/SecondAccountManagementPlugin.php
+++ PluginExample/Plugin/Customer/Model/SecondAccountManagementPlugin.php
@@ -3,10 +3,9 @@
 namespace Magestudy\PluginExample\Plugin\Customer\Model;
 
 use Magento\Customer\Model\AccountManagement;
-use Magento\Customer\Model\Data\Customer;
 use Magento\Framework\Message\ManagerInterface;
 
-class AccountManagementPlugin
+class SecondAccountManagementPlugin
 {
     /** @var ManagerInterface */
     private $messageManager;
@@ -22,31 +21,25 @@ class AccountManagementPlugin
 
     public function beforeAuthenticate(AccountManagement $subject, $username, $password)
     {
-        $this->messageManager->addErrorMessage('First Plugin: This text added before customer authenticate.');
+        $this->messageManager->addSuccessMessage('Second Plugin: This text added before customer authenticate.');
         return [$username, $password];
     }
 
     public function afterAuthenticate(AccountManagement $subject, $result)
     {
-        $this->messageManager->addErrorMessage('First Plugin: This text added after customer authenticate.');
+        $this->messageManager->addSuccessMessage('Second Plugin: This text added after customer authenticate.');
 
         return $result;
     }
 
     public function aroundAuthenticate(AccountManagement $subject, callable $proceed, $username, $password)
     {
-        $this->messageManager->addErrorMessage('First Plugin: This text added before proceed');
+        $this->messageManager->addSuccessMessage('Second Plugin: This text added before proceed');
 
         $result = $proceed($username, $password);
 
-        $this->messageManager->addErrorMessage('First Plugin: This text added after proceed');
+        $this->messageManager->addSuccessMessage('Second Plugin: This text added after proceed');
 
-        return $result;
-    }
-
-    public function afterCreateAccountWithPasswordHash(AccountManagement $subject, Customer $result)
-    {
-        $this->messageManager->addErrorMessage('This text added after customer create account.');
         return $result;
     }
 }
--- README.md
+++ README.md
@@ -96,7 +96,7 @@ magestudy:get_product
 magestudy:save_product  
 magestudy:set_product_price  
 magestudy:set_product_title  
-magestudy:set_product_image 
+magestudy:set_product_image  
 ###Cases:  
 Login as customer adds next message: "This text added before customer authenticate."  
 Create new customer: "This text added after customer create account."  
